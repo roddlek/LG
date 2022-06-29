b99dc2b508bf1ab4b4a0b8d02194b9600ef2d337
roddlek <roddlek@gmail.com>
2022-06-30 01:46:20 +0900

    lg history js-02

--- js/history.js
+++ js/history.js
@@ -10,12 +10,13 @@ $(function(){
         $(this).addClass('clicked');
         //초기 설정
         let secId = $(this).children("a").attr('href');
-        let historyH = $(secId).offset().top;
-        let tabH = $('.tabBtn').innerHeight();
-        console.log(secId, historyH, tabH)
+        //height 값 가져오기
+        let headerH = $('#History>header').outerHeight(true);
         //탭 메뉴 전환 액션
         $('#History>section').not('.tabBtn').css({display: 'none'});
         $(secId).css({display: 'block'});
+        //탭 메뉴 스크롤 액션
+        $('html, body').animate({scrollTop: headerH + 'px'}, 800);
     });
 
     ///////////페이징 버튼
@@ -25,7 +26,10 @@ $(function(){
         let historyW = $(pageNum).position().left
         console.log(pageNum, historyW)
         //페이지 이동 액션
-        $('section>div:last-child>ol').animate({marginLeft: '-=' + historyW + 'px'}, 800);
+        $('section>div:last-child>ol:not(:animated)').animate({marginLeft: '-=' + historyW + 'px'}, 1000);
+        // if(pageNum = '#1947'){
+        //     $('section>div:last-child>ol').css({marginLeft: 0})
+        // }
         return false;
     })
 })

94b187a444365c51d000528f6dcd22ca7fe2020c
roddlek <roddlek@gmail.com>
2022-08-30 17:16:45 +0900

    nav mediaquery js-3

--- js/main_common.js
+++ js/main_common.js
@@ -2,8 +2,9 @@ $(function(){
 
     // 마우스를 오버했을 때 슬라이드 다운 함수
     function navSlide(){
+        //보이지 않는 요소 처리
         $('#suNavBG').css('display', 'none');
-        //console.log(screen)
+        //
         $('.mainNav>div>nav>ul>li').on('mouseenter', function(){
             $('.mainNav').stop().animate({height: 260 + 'px'}, 600);
             $('.subNav').hide();
@@ -13,18 +14,49 @@ $(function(){
             $('.mainNav').stop().animate({height: 60 + 'px'}, 600);
         });
     }
-    function mediaQuery(){
-        console.log('hi')
+    // 반응형 - 마우스를 오버했을 때 슬라이드 다운 함수
+    function navSlideM(){
+        $('.mainNav>div>nav, #suNavBG').css('display', 'none');
+        //열기, 닫기 버튼 클릭 이벤트 함수
+        $('.mobileBtn').on('click', function(){
+            $('.mainNav>div>nav, #suNavBG').css('display', 'block');
+        });
+        $('.closeBtn').on('click', function(){
+            $('.mainNav>div>nav, #suNavBG').css('display', 'none');
+        });
+        //
+        $('.subNav').hide();
+        $('.mainNav>div>nav>ul>li').on('mouseenter', function(){
+            $(this).children('.subNav').stop().slideDown('slow');
+        });
+        $('.mainNav>div>nav>ul>li').on('mouseleave', function(){
+            $(this).children('.subNav').stop().slideUp('slow');
+        });
     }
 
     //반응형  맞춤
+    const screen = $(window).innerWidth();
+    function screenResize(){
+        $(window).on('resize', function(){
+            if(screen <= 980){
+                navSlideM();
+                //$('div>nav').load(window.location.href + ' div>nav');
+                location.reload();
+            }else{
+                navSlide();
+            }
+        })
+    }
     function screenLoad(){
-        const screen = $(window).innerWidth();
-        if(screen > 980){
-            navSlide();
-        }else{
-            mediaQuery();
-        }
+        //console.log(screen);
+        $(window).on('load', function(){
+            if(screen > 980){
+                //기존 + 리사이즈 조건 충족
+                navSlide();
+            }else{
+                navSlideM();
+            }
+        })
     }
 
     // 탑버튼 클릭했을 때 scrollTop 초기화 함수
@@ -51,6 +83,7 @@ $(function(){
 
     //navSlide();
     screenLoad();
+    //screenResize();
     topBtn();
     siteMenu();
 })
